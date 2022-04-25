e4394e86237d76454c14864061f537ce508d43f9
krishnateja <krishna.kt03@gmail.com>
2022-04-24 18:05:10 -0700

    Model simple structure

--- models/cnn_model.py
+++ models/cnn_model.py
@@ -7,7 +7,11 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 class Pneumonia_Classification_Net(nn.Module):
-    def __init__(self):
+    def __init__(self, activation, conv_layer, dense_layer, layer_size):
+        self.activation = activation
+        self.conv_layer = conv_layer
+        self.dense_layer = dense_layer
+        self.layer_size = layer_size
         return
 
     def build_model(self):
