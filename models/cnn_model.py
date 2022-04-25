339c1de5978a37787b25da58417fe0dca6fb096b
krishnateja <krishna.kt03@gmail.com>
2022-04-24 18:03:50 -0700

    Model simple structure

---
+++ models/cnn_model.py
@@ -0,0 +1,14 @@
+'''
+CNN model for Pneumonia classification
+'''
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class Pneumonia_Classification_Net(nn.Module):
+    def __init__(self):
+        return
+
+    def build_model(self):
+        return
